4d87971ac3b8508f418834e5cdff9487ade2e123
Parth <devparthmandale915@gmail.com>
2023-07-14 01:07:29 +0530

    first commit

---
+++ src/component/Field.jsx
@@ -0,0 +1,19 @@
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+// import DeleteIcon from '@mui/icons-material/Delete';
+
+// const Field = ({name,email,index}) => {
+//   return (
+    
+//     <div className='data_val'>
+//     <h1>{name}</h1>
+//     <h1>{email}</h1>
+//     <Button variant="contained" color="error">
+//     <DeleteIcon  />
+//       </Button>
+//   </div>
+
+//   )
+// }
+
+// export default Field
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+    
+        <h1>Record-Keeping</h1>
+        
+    </div>
+  )
+}
+
+export default Header
